3922e97c635b54237eafdc028d96c967f1f49572
vuvuvo0303 <levanhoangvu2003@gmail.com>
2024-10-07 22:41:19 +0700

    config api rooms

---
+++ src/types/room.ts
@@ -0,0 +1,10 @@
+export type Room = {
+  id: number;
+  roomName: string;
+  capacity: number;
+  status: string;
+  roomTypeId:number;
+  createdAt: string;
+  deletedAt?: string;
+  updatedAt: string;
+};
