c4a2ea0802057b16c60d4d52805a4a41408527f4
vuvuvo0303 <levanhoangvu2003@gmail.com>
2024-10-15 14:59:20 +0700

    done success page in step-process

--- src/pages/student/step-process/Done.tsx
+++ src/pages/student/step-process/Done.tsx
@@ -1,8 +1,18 @@
+import success from "../../../assets/success.json"; // Thay đường dẫn đến file Lottie animation
+import Lottie from "lottie-react";
 
 const Done = () => {
   return (
-    <div>Done</div>
-  )
-}
+    <div className="bg-white">
+      <div className="flex flex-col items-center py-36">
+        <Lottie animationData={success} loop={false} className="w-60 h-60 m-0" />
 
-export default Done
+        <span className="text-4xl h-12 font-semibold bg-gradient-to-r from-green-500 to-green-400 bg-clip-text text-transparent">
+          Booking Successfully!!!
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Done;
